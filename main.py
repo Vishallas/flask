faeb4419f714091f237645e1bc65695cb0665b1a
Vishal <103063354+Vishallas@users.noreply.github.com>
2023-04-14 18:28:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from flask import Flask, jsonify
-import os
+#!/usr/bin/env python
+# encoding: utf-8
 
+from flask import Flask, jsonify,request
 app = Flask(__name__)
-
+#import os
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route('/name')
+def index():
+    name = request.args.get('name')
+    #email = request.args.get('email')
+    return jsonify({'name': f'{name}'})
+                    #'email': f'{email}'})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=5000,host="0.0.0.0")
